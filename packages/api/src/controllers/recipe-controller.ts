783c1604f9beab188ce5f3a3b64af808a78a6c70
jmg1138 <jmg1138@users.noreply.github.com>
2023-10-22 01:56:53 -0700

    Fix linting.

--- packages/api/src/controllers/recipe-controller.ts
+++ packages/api/src/controllers/recipe-controller.ts
@@ -23,174 +23,173 @@
  *
  * @module
  */
- import type { IBaseController } from '@recipe-report/api/controllers'
- import { Responder } from '@recipe-report/api/controllers'
- import { SYMBOLS } from '@recipe-report/api/ioc'
- import type { RequestWithUser } from '@recipe-report/api/middlewares'
- import { fiveHundred, tokenwall } from '@recipe-report/api/middlewares'
- import { httpStatus, outcomes } from '@recipe-report/data'
- import { Err, errClient, isErrClient } from '@recipe-report/domain/models'
- import { RecipeRequest, UuidRequest } from '@recipe-report/domain/services'
- import type { IRecipeService } from '@recipe-report/service'
- import type { NextFunction, Response } from 'express'
- import { Router } from 'express'
- import { inject, injectable } from 'inversify'
- import 'reflect-metadata'
- 
- @injectable()
- export class RecipeController implements IBaseController {
-   private _recipeService: IRecipeService
-   router: Router = Router()
-   path: string = `/v1/recipes`
- 
-   public constructor(@inject(SYMBOLS.IRecipeService) recipeService: IRecipeService) {
-     this._recipeService = recipeService
-     this.initRoutes()
-   }
- 
-   public initRoutes = (): void => {
-     this.router.post(`/`, tokenwall, this.create)
-     this.router.get(`/:id`, tokenwall, this.read)
-     this.router.put(`/:id`, tokenwall, this.update)
-     this.router.delete(`/:id`, tokenwall, this.delete)
-     this.router.use(fiveHundred) // Error handling.
-   }
- 
-   private create = async (
-     req: RequestWithUser,
-     res: Response,
-     next: NextFunction,
-   ): Promise<void> => {
-     try {
-       const svcReq = RecipeRequest.create({ ...req.body })
-       const svcRes = await this._recipeService.create(svcReq)
-       const code = svcRes.statusCode
-       switch (svcRes.outcome) {
-         case outcomes.SUCCESS:
-           Responder.success(res, code, { recipe: svcRes.item })
-           break
-         case outcomes.FAIL:
-           Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-         default:
-           Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-       }
-     } catch (e) {
-       // The caught e could be anything. Turn it into an Err.
-       const err = Err.toErr(e)
-       // If the error message can be client facing, return BAD_REQUEST.
-       if (isErrClient(err.name)) {
-         err.message = `${errClient.RECIPE_CREATE} ${err.message}`
-         Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
-       } else {
-         next(err)
-       }
-     }
-   }
- 
-   private read = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
-     try {
-       if (!req.params['id']) {
-         throw new Err('UID_INVALID', errClient.UID_INVALID)
-       }
-       const svcReq = UuidRequest.create(req.params['id'], req.authorizedId)
-       const svcRes = await this._recipeService.read(svcReq)
-       const code = svcRes.statusCode
-       switch (svcRes.outcome) {
-         case outcomes.SUCCESS:
-           Responder.success(res, code, { recipe: svcRes.item })
-           break
-         case outcomes.FAIL:
-           Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-         default:
-           Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-       }
-     } catch (e) {
-       // The caught e could be anything. Turn it into an Err.
-       const err = Err.toErr(e)
-       // If the error message can be client facing, return BAD_REQUEST.
-       if (isErrClient(err.name)) {
-         err.message = `${errClient.RECIPE_READ} ${err.message}`
-         Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
-       } else {
-         next(err)
-       }
-     }
-   }
- 
-   private update = async (
-     req: RequestWithUser,
-     res: Response,
-     next: NextFunction,
-   ): Promise<void> => {
-     try {
-       if (req.params['id'] !== req.body.id) {
-         throw new Err(`ID_MISMATCH`, errClient.ID_MISMATCH)
-       }
-       const svcReq = RecipeRequest.create({ ...req.body }, req.authorizedId)
-       const svcRes = await this._recipeService.update(svcReq)
-       const code = svcRes.statusCode
-       switch (svcRes.outcome) {
-         case outcomes.SUCCESS:
-           Responder.success(res, code, { recipe: svcRes.item })
-           break
-         case outcomes.FAIL:
-           Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-         default:
-           Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-       }
-     } catch (e) {
-       // The caught e could be anything. Turn it into an Err.
-       const err = Err.toErr(e)
-       // If the error message can be client facing, return BAD_REQUEST.
-       if (isErrClient(err.name)) {
-         err.message = `${errClient.RECIPE_UPDATE} ${err.message}`
-         Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
-       } else {
-         next(err)
-       }
-     }
-   }
- 
-   private delete = async (
-     req: RequestWithUser,
-     res: Response,
-     next: NextFunction,
-   ): Promise<void> => {
-     try {
-       if (!req.params['id']) {
-         throw new Err('UID_INVALID', errClient.UID_INVALID)
-       }
-       const svcReq = UuidRequest.create(req.params['id'], req.authorizedId)
-       const svcRes = await this._recipeService.delete(svcReq)
-       const code = svcRes.statusCode
-       switch (svcRes.outcome) {
-         case outcomes.SUCCESS:
-           Responder.success(res, code, { recipe: svcRes.item })
-           break
-         case outcomes.FAIL:
-           Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-         default:
-           Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
-           break
-       }
-     } catch (e) {
-       // The caught e could be anything. Turn it into an Err.
-       const err = Err.toErr(e)
-       // If the error message can be client facing, return BAD_REQUEST.
-       if (isErrClient(err.name)) {
-         err.message = `${errClient.RECIPE_DELETE} ${err.message}`
-         Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
-       } else {
-         next(err)
-       }
-     }
-   }
- }
- 
+import type { IBaseController } from '@recipe-report/api/controllers'
+import { Responder } from '@recipe-report/api/controllers'
+import { SYMBOLS } from '@recipe-report/api/ioc'
+import type { RequestWithUser } from '@recipe-report/api/middlewares'
+import { fiveHundred, tokenwall } from '@recipe-report/api/middlewares'
+import { httpStatus, outcomes } from '@recipe-report/data'
+import { Err, errClient, isErrClient } from '@recipe-report/domain/models'
+import { RecipeRequest, UuidRequest } from '@recipe-report/domain/services'
+import type { IRecipeService } from '@recipe-report/service'
+import type { NextFunction, Response } from 'express'
+import { Router } from 'express'
+import { inject, injectable } from 'inversify'
+import 'reflect-metadata'
+
+@injectable()
+export class RecipeController implements IBaseController {
+  private _recipeService: IRecipeService
+  router: Router = Router()
+  path: string = `/v1/recipes`
+
+  public constructor(@inject(SYMBOLS.IRecipeService) recipeService: IRecipeService) {
+    this._recipeService = recipeService
+    this.initRoutes()
+  }
+
+  public initRoutes = (): void => {
+    this.router.post(`/`, tokenwall, this.create)
+    this.router.get(`/:id`, tokenwall, this.read)
+    this.router.put(`/:id`, tokenwall, this.update)
+    this.router.delete(`/:id`, tokenwall, this.delete)
+    this.router.use(fiveHundred) // Error handling.
+  }
+
+  private create = async (
+    req: RequestWithUser,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const svcReq = RecipeRequest.create({ ...req.body })
+      const svcRes = await this._recipeService.create(svcReq)
+      const code = svcRes.statusCode
+      switch (svcRes.outcome) {
+        case outcomes.SUCCESS:
+          Responder.success(res, code, { recipe: svcRes.item })
+          break
+        case outcomes.FAIL:
+          Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+        default:
+          Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+      }
+    } catch (e) {
+      // The caught e could be anything. Turn it into an Err.
+      const err = Err.toErr(e)
+      // If the error message can be client facing, return BAD_REQUEST.
+      if (isErrClient(err.name)) {
+        err.message = `${errClient.RECIPE_CREATE} ${err.message}`
+        Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
+      } else {
+        next(err)
+      }
+    }
+  }
+
+  private read = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      if (!req.params['id']) {
+        throw new Err('UID_INVALID', errClient.UID_INVALID)
+      }
+      const svcReq = UuidRequest.create(req.params['id'], req.authorizedId)
+      const svcRes = await this._recipeService.read(svcReq)
+      const code = svcRes.statusCode
+      switch (svcRes.outcome) {
+        case outcomes.SUCCESS:
+          Responder.success(res, code, { recipe: svcRes.item })
+          break
+        case outcomes.FAIL:
+          Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+        default:
+          Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+      }
+    } catch (e) {
+      // The caught e could be anything. Turn it into an Err.
+      const err = Err.toErr(e)
+      // If the error message can be client facing, return BAD_REQUEST.
+      if (isErrClient(err.name)) {
+        err.message = `${errClient.RECIPE_READ} ${err.message}`
+        Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
+      } else {
+        next(err)
+      }
+    }
+  }
+
+  private update = async (
+    req: RequestWithUser,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      if (req.params['id'] !== req.body.id) {
+        throw new Err(`ID_MISMATCH`, errClient.ID_MISMATCH)
+      }
+      const svcReq = RecipeRequest.create({ ...req.body }, req.authorizedId)
+      const svcRes = await this._recipeService.update(svcReq)
+      const code = svcRes.statusCode
+      switch (svcRes.outcome) {
+        case outcomes.SUCCESS:
+          Responder.success(res, code, { recipe: svcRes.item })
+          break
+        case outcomes.FAIL:
+          Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+        default:
+          Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+      }
+    } catch (e) {
+      // The caught e could be anything. Turn it into an Err.
+      const err = Err.toErr(e)
+      // If the error message can be client facing, return BAD_REQUEST.
+      if (isErrClient(err.name)) {
+        err.message = `${errClient.RECIPE_UPDATE} ${err.message}`
+        Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
+      } else {
+        next(err)
+      }
+    }
+  }
+
+  private delete = async (
+    req: RequestWithUser,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      if (!req.params['id']) {
+        throw new Err('UID_INVALID', errClient.UID_INVALID)
+      }
+      const svcReq = UuidRequest.create(req.params['id'], req.authorizedId)
+      const svcRes = await this._recipeService.delete(svcReq)
+      const code = svcRes.statusCode
+      switch (svcRes.outcome) {
+        case outcomes.SUCCESS:
+          Responder.success(res, code, { recipe: svcRes.item })
+          break
+        case outcomes.FAIL:
+          Responder.fail(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+        default:
+          Responder.error(res, code, svcRes.err?.message, svcRes.err?.name)
+          break
+      }
+    } catch (e) {
+      // The caught e could be anything. Turn it into an Err.
+      const err = Err.toErr(e)
+      // If the error message can be client facing, return BAD_REQUEST.
+      if (isErrClient(err.name)) {
+        err.message = `${errClient.RECIPE_DELETE} ${err.message}`
+        Responder.fail(res, httpStatus.BAD_REQUEST, err.message, err.name)
+      } else {
+        next(err)
+      }
+    }
+  }
+}
